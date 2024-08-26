1c6dba5e4093325cb900c211557afee0ef2e1cb7
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-08-26 10:52:00 +0300

    create ClientController.java & PetController.java

--- src/main/java/com/handroid/vet_clinic/controller/ClientController.java
+++ src/main/java/com/handroid/vet_clinic/controller/ClientController.java
@@ -5,7 +5,12 @@ import com.handroid.vet_clinic.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
